f34f7ede9baadb3c3e3cef66bc04fdefbfb6a54b
Fea Hsu <fea.x.cn@gmail.com>
2021-08-20 15:11:36 +0800

    就是玩

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -23,7 +23,7 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, onMounted, ref, Ref } from 'vue';
+import { computed, defineComponent, onMounted, ref } from 'vue';
 import { useRoute } from 'vue-router';
 
 export default defineComponent({
